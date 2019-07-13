c8fd6f81122f424e5f51063a7c3b0de46638f002
Martin Matovu <matovu.synergy@gmail.com>
2019-07-13 16:59:38 +0300

    Add base_path, and declare variable SITE_PAGES_DIRECTORY, for path to look into "pages" folder

--- prototypes.py
+++ prototypes.py
@@ -4,6 +4,8 @@ import sys
 from django.conf import settings
 
 
+BASE_DIR = os.path.dirname(__file__)
+
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 
 settings.configure(
@@ -15,6 +17,7 @@ settings.configure(
         'sitebuilder',
     ),
     STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages')
 )
 
 
