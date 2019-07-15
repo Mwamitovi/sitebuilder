54bb9a4fdb27c45e461a95ef68c6618481c39b25
Martin Matovu <matovu.synergy@gmail.com>
2019-07-15 20:37:21 +0300

    Update files to pep8 spacing

--- prototypes.py
+++ prototypes.py
@@ -44,35 +44,3 @@ settings.configure(
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
